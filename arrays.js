bf1f21cd410b9dd42df7708f0db0d42b56ad5d10
sperrysz39@gmail.com <sperrysz39@gmail.com>
2018-03-12 20:35:49 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,47 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+  ]
+  
+  function addElementToBeginningOfArray(array, element) {
+  var array = [element, ...array]
+  return array
+  }
+  
+  function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+  var array = [...array, element]
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index){
+ return array[index]
+}
+
+
+function destructivelyRemoveElementFromBeginningOfArray(array, element){
+  return array.shift(element);
+}
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array, element) {
+  return array.pop(element);
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length-1);
+}
